359c6ba56b10b12d8ad73d7ca4400974f1843236
michael_canavan <canavm2@gmail.com>
2022-04-15 20:32:12 -0600

    make functions, clean code

---
+++ utils/data_prep_helper.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def prep_data(WideDF, sectors, df500sectors):
+    WideDF.drop(columns='index', inplace=True)
+    WideDF = WideDF.set_index('timestamp')
+    SectorDF = WideDF[['SPY', 'SPY Open', 'SPY Close']]
+    stockstodrop = []
+    StocksWideNoSPY = WideDF.drop(columns={'SPY','SPY Open','SPY Close'})
+    for sector in sectors:
+        for (columnName, columnValues) in StocksWideNoSPY.iteritems():
+            if sector != df500sectors.loc[columnName]['GICS Sector']:
+                stockstodrop.append(columnName)
+        TempDF = StocksWideNoSPY.drop(columns=stockstodrop)
+        stockstodrop = []
+        SectorDF[sector] = TempDF.mean(axis=1)
+    return SectorDF
