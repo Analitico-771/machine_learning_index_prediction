d2b9814f36e77aeff3465126793fc81693001f1d
analiticofx <79728365+analiticofx@users.noreply.github.com>
2022-04-19 00:21:23 -0400

    refactored the train_split code to use the default split. cleaned up comments and deleted unused code for all files. added metrics and graphics to jupiter notebook

--- utils/random_forest.py
+++ utils/random_forest.py
@@ -8,6 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 from pandas.tseries.offsets import DateOffset
 from sklearn.inspection import permutation_importance
+from sklearn.model_selection import train_test_split
 # from sklearn.metrics import classification_report
 
 
@@ -16,27 +17,8 @@ def get_train_split(X, y):
     Get the train split for the machine learning model and scales the data.
     Returns scaled and non-scaled trained and test data.
     """
-    # Select the start of the training period
-    training_begin = X.index.min()
-
-    # Display the training begin date
-    print(training_begin)
-    # Select the ending period for the training data with an offset of 3 months
-    training_end = X.index.min() + DateOffset(months=6)
-
-    # Display the training end date
-    print(training_end)
-
-    # Generate the X_train and y_train DataFrames
-    X_train = X.loc[training_begin:training_end]
-    y_train = y.loc[training_begin:training_end]
-
-    # Display sample data
-    X_train.head()
-
-    # Generate the X_test and y_test DataFrames
-    X_test = X.loc[training_end:]
-    y_test = y.loc[training_end:]
+    # Select the split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
     # Display sample data
     X_test.head()
@@ -67,11 +49,11 @@ def get_importance(train_split, X):
     Returns the RandomForestClassifier model instance.
     """
     X_test, X_train_scaled, X_test_scaled, y_train, y_test = train_split.values()
-    # Scaled data
-    rdm_forest_model = RandomForestClassifier(max_depth=10, random_state=None)
+    # Create an instance of the model
+    rdm_forest_model = RandomForestClassifier(max_depth=5, random_state=None)
     rdm_forest_model.fit(X_train_scaled, np.ravel(y_train, order='c'), sample_weight=None)
-    # rdm_forest_model.fit(X_train_scaled, y_train.values.ravel(), sample_weight=None)
-    feat_importance = rdm_forest_model.feature_importances_
+    # analyze the feature importance values
+    feat_importances = rdm_forest_model.feature_importances_
     count = 0
     X_new = X.copy()
     X_new_cols = X_new.columns.to_list()
@@ -81,8 +63,9 @@ def get_importance(train_split, X):
     print('X_new_cols', X_new_cols)
     columns_to_drop = []
     dropped_feature_importances = []
+    # print(np.mean(feat_importances))
     # Check for importance level and remove cols from df below threshold
-    for each_feat in feat_importance:
+    for each_feat in feat_importances:
         if each_feat < 0.085:
             dropped_feature_importances.append(each_feat)
             columns_to_drop.append(X_new_cols[count])
@@ -91,12 +74,13 @@ def get_importance(train_split, X):
         count = count + 1
 
     # check new X df for accuracy
-    print('feat_importance\n', feat_importance)
+    print('feat_importance\n', feat_importances)
     print('dropped_feature_importances\n', dropped_feature_importances)
     print('dropped_X_columns\n', columns_to_drop)
     
     # Return the model and the new X df with optimized important columns
     return {
+        'feat_importances': feat_importances,
         'rdm_forest_model': rdm_forest_model,
         'X_new': X_new
     }
